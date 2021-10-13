42537504ef2d0352f426326e6a8a8212f3a811ad
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-13 16:03:56 +0200

    almost fixed for

--- asset/main.js
+++ asset/main.js
@@ -1,8 +1,17 @@
 let list = document.querySelector('.list')
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    const fizz = `<li class="square lightgreen"> fizz </li>`
+
+    list.innerHTML += fizz
+  } else if (i % 5 === 0) {
+    const buzz = `<li class="square yellow"> fizz </li>`
+
+    list.innerHTML += buzz
+  }
   console.log(i);
-  const element = `<li class="square"> ${i} </li>`
+  const element = `<li class="square blue"> ${i} </li>`
 
   list.innerHTML += element
 }
