a2a62206c284d8dd9b86531be03a4a1099ae3801
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-13 16:14:59 +0200

    finish exercize

--- asset/main.js
+++ asset/main.js
@@ -1,17 +1,24 @@
 let list = document.querySelector('.list')
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    const fizz = `<li class="square lightgreen"> fizz </li>`
+  if (i % 15 === 0) {
+    const element = `<li class="square red"> fizzBuzz </li>`
 
-    list.innerHTML += fizz
+    list.innerHTML += element
+
   } else if (i % 5 === 0) {
-    const buzz = `<li class="square yellow"> fizz </li>`
+    const element = `<li class="square yellow"> buzz </li>`
 
-    list.innerHTML += buzz
+    list.innerHTML += element
+
+  } else if (i % 3 === 0) {
+    const element = `<li class="square lightgreen"> fizz </li>`
+
+    list.innerHTML += element
+  } else {
+
+    const element = `<li class="square blue"> ${i} </li>`
+
+    list.innerHTML += element
   }
-  console.log(i);
-  const element = `<li class="square blue"> ${i} </li>`
-
-  list.innerHTML += element
 }
